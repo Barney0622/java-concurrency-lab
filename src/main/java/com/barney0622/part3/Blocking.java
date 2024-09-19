d8f4d08b6ead362eab5bf5beb22a3a711b4b6a50
Barney <nn637159@gmail.com>
2024-09-19 16:24:30 +0800

    chapter3

---
+++ src/main/java/com/barney0622/part3/Blocking.java
@@ -0,0 +1,23 @@
+package com.barney0622.part3;
+
+public class Blocking {
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(new BlockingTask());
+
+        thread.start();
+        thread.interrupt();
+    }
+
+    private static class BlockingTask implements Runnable {
+
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(5000000);
+            } catch (InterruptedException e) {
+                System.out.println("Exiting blocking thread");
+            }
+        }
+    }
+}
